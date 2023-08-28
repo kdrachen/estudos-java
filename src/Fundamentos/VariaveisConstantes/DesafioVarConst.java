4409a6d0e23cbc55084c38ba8bfa825536c145b5
liweisen119 <natan_will88@hotmail.com>
2023-08-28 18:31:14 -0300

    Criação do primeiro projeto.

---
+++ src/Fundamentos/VariaveisConstantes/DesafioVarConst.java
@@ -0,0 +1,17 @@
+package Fundamentos.VariaveisConstantes;
+
+public class DesafioVarConst {
+    public static void main(String[] args) {
+        /*
+        * Desafio - Temperatura
+        * Converção - (°F - 32) x 5/9 = °C
+        * */
+        final int AJUSTE = 32;
+        final double FATOR = 5 / 9.0;
+        int TemperaturaFahrenheit = 56;
+        double Celsius = (TemperaturaFahrenheit - AJUSTE) * FATOR;
+
+        System.out.println("A conversão de Fahrenheit para Celsius é: " + Celsius + "°C");
+
+    }
+}
