e1b89c136b0fec835591979065d3f4c755bf0a0e
liweisen119 <natan_will88@hotmail.com>
2023-08-31 15:31:31 -0300

    29/08/2023

--- src/Fundamentos/TipoString.java
+++ src/Fundamentos/TipoString.java
@@ -4,7 +4,8 @@ public class TipoString {
     public static void main(String[] args) {
         String nome = "Natã";
         String sobrenome = "Wilian";
-        int idade = 19;
+        var idade = 19;
+        var id = 1230123.124123;
         System.out.println(nome.charAt(0));
 
         String s = "Boa tarde";
@@ -15,6 +16,11 @@ public class TipoString {
         System.out.println(s.toLowerCase().equals("boa tarde"));
         System.out.println(s.equalsIgnoreCase("boa tarde"));
         System.out.println("Nome: " + nome + "\nSobrenome: " + sobrenome + "\nIdade: " + idade);
-        System.out.printf("Nome: %s, idade: %s", nome, idade);
+        System.out.printf("Nome: %s, idade: %s, salário %.2f", nome, idade, id); // %f para usar .flutuante
+
+        String frase = String.format("\nNome: %s, idade: %s, salário %.2f", nome, idade, id);
+        System.out.println(frase);
+        System.out.println(s.substring(2));
+        System.out.println(s.substring(5, 7));
     }
 }
