9103748bb561fc07804c2331b766361da8f7d4b4
liweisen119 <natan_will88@hotmail.com>
2023-09-02 15:50:58 -0300

    02/09/2023

--- src/Fundamentos/Conversão.java
+++ src/Fundamentos/Conversão.java
@@ -2,7 +2,18 @@ package Fundamentos;
 
 public class Conversão {
     public static void main(String[] args) {
+        double a = 1; // Conversão implícita
+        System.out.println(a);
 
+        float b = (float) 1.12345; // Conversão explícita (CAST)
+        System.out.println(b);
 
+        int c = 140;
+        byte d = (byte) c; // Conversão explícita (CAST)
+        System.out.println(d);
+
+        double e = 1.999;
+        int f = (int) e; // Conversão explícita (CAST)
+        System.out.println(f);
     }
 }
