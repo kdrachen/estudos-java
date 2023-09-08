766558fc05fd52b9b29558f12b3cebb99a91e156
liweisen119 <natan_will88@hotmail.com>
2023-09-08 11:21:20 -0300

    08/09/2023

--- src/EstruturasControle/Desafios/DiaSemana.java
+++ src/EstruturasControle/Desafios/DiaSemana.java
@@ -21,7 +21,7 @@ public class DiaSemana {
             System.out.println(1);
         } else if(dia.equalsIgnoreCase("segunda")) {
             System.out.println(2);
-        } else if(dia.equalsIgnoreCase("terça")) {
+        } else if(dia.equalsIgnoreCase("terça") || dia.equalsIgnoreCase("terca")) {
             System.out.println(3);
         } else if(dia.equalsIgnoreCase("quarta")) {
             System.out.println(4);
@@ -29,7 +29,7 @@ public class DiaSemana {
             System.out.println(5);
         } else if(dia.equalsIgnoreCase("sexta")) {
             System.out.println(6);
-        } else if(dia.equalsIgnoreCase("sábado")) {
+        } else if(dia.equalsIgnoreCase("sábado") || dia.equalsIgnoreCase("sabado")) {
             System.out.println(7);
         } else {
             System.out.println("Digite um dia da semana válido.");
