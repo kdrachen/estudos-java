bb4592f6687297227f47fe1925018b82c0fe47d2
liweisen119 <natan_will88@hotmail.com>
2023-09-09 13:37:36 -0300

    09/09/2023

---
+++ src/EstruturasControle/SwitchSemBreak.java
@@ -0,0 +1,43 @@
+package EstruturasControle;
+
+public class SwitchSemBreak {
+
+    public static void main(String[] args) {
+
+        // if(bool) ...
+        // while(bool) ...
+        // for(;bool;) ...
+
+        String faixa = "vermelha";
+
+        switch (faixa.toLowerCase()) {
+            case "preta":
+                System.out.println("Sei o Bassai-Dai...");
+            case "marrom":
+                System.out.println("Sei o Tekki Shodan");
+            case "roxa":
+                System.out.println("Sei o Heian Godan");
+            case "verde":
+                System.out.println("Sei o Heian Yodan");
+            case "laranja":
+                System.out.println("Sei o Heian Sandan");
+            case "vermelha":
+                System.out.println("Sei o Heian Nidan");
+            case "amarela":
+                System.out.println("Sei o Heian Shodan");
+                break;
+            default:
+                System.out.println("Não sei de nada");
+        }
+
+        int idade = 2;
+        switch (idade) {
+            case 3:
+                System.out.println("Sabe falar");
+            case 2:
+                System.out.println("Sabe andar");
+            case 1:
+                System.out.println("Sabe respirar");
+        }
+    }
+}
