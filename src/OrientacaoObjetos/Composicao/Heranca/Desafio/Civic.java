4c3ba68b8167d575743daacdada3dfbbec8da970
liweisen119 <natan_will88@hotmail.com>
2023-10-02 13:59:09 -0300

    02/10/2023 - Desafio Herança

--- src/OrientacaoObjetos/Composicao/Heranca/Desafio/Civic.java
+++ src/OrientacaoObjetos/Composicao/Heranca/Desafio/Civic.java
@@ -2,6 +2,7 @@ package OrientacaoObjetos.Composicao.Heranca.Desafio;
 
 public class Civic extends Carro {
 
+    @Override
     void acelerar() {
         super.acelerar();
         super.velAtual += 10;
--- src/OrientacaoObjetos/Composicao/Heranca/Desafio/Ferrari.java
+++ src/OrientacaoObjetos/Composicao/Heranca/Desafio/Ferrari.java
@@ -2,6 +2,7 @@ package OrientacaoObjetos.Composicao.Heranca.Desafio;
 
 public class Ferrari extends Carro {
 
+    @Override
     void acelerar() {
         super.acelerar();
         super.velAtual += 15;
