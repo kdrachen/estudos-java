276f5b3e05d5e6cce4fc27293e85a80ecfe4c8a4
liweisen119 <natan_will88@hotmail.com>
2023-10-02 13:47:55 -0300

    02/10/2023 - Herança

--- src/OrientacaoObjetos/Composicao/Heranca/Heroi.java
+++ src/OrientacaoObjetos/Composicao/Heranca/Heroi.java
@@ -2,5 +2,10 @@ package OrientacaoObjetos.Composicao.Heranca;
 
 public class Heroi extends Jogador {
 
-    
+    boolean atacar(Jogador oponente) {
+        boolean ataque1 = super.atacar(oponente);
+        boolean ataque2 = super.atacar(oponente);
+        boolean ataque3 = super.atacar(oponente);
+        return ataque1 || ataque2 || ataque3;
+    }
 }
