08b60c641414272c8a02e41bab4dd1043b3fae69
liweisen119 <natan_will88@hotmail.com>
2023-10-08 13:34:51 -0300

    Lambdas - Method Reference

--- src/Lambdas/Foreach/Foreach.java
+++ src/Lambdas/Foreach/Foreach.java
@@ -19,5 +19,15 @@ public class Foreach {
 
         System.out.println("\n\nMethod Reference...");
         aprovados.forEach(System.out::println);
+
+        System.out.println("\nLambda #02...");
+        aprovados.forEach(nome ->  meuImprimir(nome));
+
+        System.out.println("\nMethod Reference #2...");
+        aprovados.forEach(Foreach::meuImprimir);
+    }
+
+    static void meuImprimir(String nome) {
+        System.out.println("Oi! Meu nome é " + nome);
     }
 }
