1b078cc468672f2b9d0948084f5243f89ab1154d
liweisen119 <natan_will88@hotmail.com>
2023-10-13 13:51:14 -0300

    Desafio Stream API e MAP

--- src/StreamAPI/DesafioMap/DesMap.java
+++ src/StreamAPI/DesafioMap/DesMap.java
@@ -9,13 +9,14 @@ import java.util.function.UnaryOperator;
 public class DesMap {
 
     public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         /*
         * 1. Número para String binária... 6 => "110"
         * 2. Reverter a string... "110" => "011"
         * 3. Converter de volta para inteiro => "011" => 3
         * */
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         System.out.print("1 - ");
         nums.stream().map(toString).forEach(n -> System.out.print(n + " "));
