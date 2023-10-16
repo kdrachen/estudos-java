39c4afc3f452e82cf58eea865d1eae2a2396f329
liweisen119 <natan_will88@hotmail.com>
2023-10-16 14:29:22 -0300

    Reduce

---
+++ src/StreamAPI/Reduce/Aluno.java
@@ -0,0 +1,18 @@
+package StreamAPI.Reduce;
+
+public class Aluno {
+
+    final String nome;
+    double nota;
+    final boolean bomComportamento;
+
+    public Aluno(String nome, double nota) {
+        this(nome, nota, true);
+    }
+
+    public Aluno(String nome, double nota, boolean bomComportamento) {
+        this.nome = nome;
+        this.nota = nota;
+        this.bomComportamento = bomComportamento;
+    }
+}
